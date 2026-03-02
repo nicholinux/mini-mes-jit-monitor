c78bc106c3525710dca5d851d5315afbc1188fd3
Nicholas Hubbard <nickhubbard17@gmail.com>
2026-03-02 16:19:21 -0500

    Refactor Program.cs to include Main method

--- Program.cs
+++ Program.cs
@@ -11,3 +11,33 @@ public class LogEvent
     public string EventType { get; set; } = "";
     public string ReasonCode { get; set; } = ""; 
 }
+
+class Program
+{
+    static void Main()
+    {
+        string path = "sample_log.csv";
+        string[] lines = File.ReadAllLines(path);
+
+        List<LogEvent> events = new List<LogEvent>();
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+           string line = lines[i];
+           string[] parts = line.Split(',');
+
+           LogEvent e = new LogEvent
+           {
+               TimeStamp = DateTime.Parse(parts[0]),
+               LineId = parts[1],
+               StationID = parts[2],
+               UnitID = parts[3],
+               EventType = parts[4],
+               ReasonCode = parts.Length > 5 ? parts[5] : ""
+           };
+            
+           events.Add(e);
+            
+        }
+    }
+}
