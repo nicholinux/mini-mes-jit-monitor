535d4bb9b4217d53918d113396baa16e919a90dc
Nicholas Hubbard <nickhubbard17@gmail.com>
2026-03-02 15:49:49 -0500

    Add LogEvent class and necessary using directives

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System; // needed to interface with files
+using System.Collections.Generic;
+using System.IO;
+
+public class LogEvent
+{
+    public DateTime TimeStamp { get; set; }
+    public string LineId{ get; set; } = "";
+    public string StationID { get; set; } = "";
+    public string UnitID { get; set; } = "";
+    public string EventType { get; set; } = "";
+    public string ReasonCode { get; set; } = ""; 
+}
